db6b15944813b1d70399c2bde4704eb447b4f9f9
Miika Pesonen <n0pemi00.students.oamk.fi>
2021-10-10 22:57:31 +0300

    exchange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+const URL = 'http://api.exchangeratesapi.io/v1/latest?access_key='
+const API_KEY = '121567e1abfdefc9634150a23fba0f69'
 
 function App() {
+  const [eur,setEur] = useState(0);
+  const [gdp,setGbp] = useState(0);
+  const [rate,setRate] = useState(0); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <form onSubmit={convert}>
+        <div>
+          <label>Eur</label>&nbsp;
+          <input type="number" step="0.01"
+          value={eur} onChange={e => setEur(e.target.value)}/>
+          <output>{rate}</output>
+        </div>
+        <div>
+          <label>Gdp</label>
+          <output>{gdp.toFixed(2)} €</output>
+        </div>
+        <div>
+          <button>Calculate</button>
+        </div>
+      </form>
     </div>
   );
+
+  async function convert(e) {
+    e.preventDefault();
+    try{
+      const address = URL + API_KEY
+      const response = await fetch(address)
+  
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json.rates.GBP)
+        setRate(json.rates.GBP);
+  
+        setGbp(eur * json.rates.GBP);
+      } else {
+        alert('Error retrieving exhange rate');
+        console.log(response)
+      }
+    } catch (err) {
+      alert(err);
+    }
+  }
 }
 
 export default App;
